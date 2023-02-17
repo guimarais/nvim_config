85d54909860a2b4082417498f311cb41c97c3d06
Luis Guimarais <luis.guimarais@volteuropa.org>
2023-02-17 00:03:39 +0000

    minor refinements

--- lua/core/plugin_config/treesitter.lua
+++ lua/core/plugin_config/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
     -- A list of parser names, or "all"
-    ensure_installed = { "c", "lua", "rust", "ruby", "vim" },
+    ensure_installed = { "c", "lua", "rust", "r", "ruby", "python" },
 
     -- Install parsers synchronously
     sync_install = false,
