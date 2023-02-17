89f7fb8e5250fa304011e4150bf2601d9daa3190
Luis Guimarais <luis.guimarais@gmail.com>
2023-02-17 11:50:54 +0000

    Update testpython.lua

--- testpython.lua
+++ testpython.lua
@@ -1,5 +1,13 @@
 -- Generated with ChatGPT, prompt:
 -- Write a neovim configuration file that autocompletes python code using the lua language, please.
+-- This configuration is written in Lua and does the same things as the previous Vimscript configuration.
+-- It sets the filetype to python for all files with the .py extension, and then sets the path to the Python interpreter and the virtual environment.
+-- It enables filetype-specific indentation and syntax highlighting and sets the omnifunc option to pythoncomplete#Complete, which enables autocompletion for Python code.
+
+-- Finally, the configuration maps the <Tab> key to trigger autocompletion using the Ctrl-x Ctrl-o key sequence using the nvim_set_keymap API function.
+
+-- Note that this configuration assumes that you have installed the python3 package and the neovim Python package, which provides the pythoncomplete module used for autocompletion.
+-- Additionally, it assumes that you have the nvim-lua plugin installed to enable Lua configuration in Neovim.
 -- Use Python syntax highlighting for all .py files
 vim.cmd('autocmd BufRead,BufNewFile *.py set filetype=python')
 
